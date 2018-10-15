78bb0f047475dd3a4dc4619f7039a27af659fc5f
sfauvel <sfauvel@gmail.com>
2018-10-16 00:08:04 +0200

    Remove dependency to org.json

--- src/main/java/org/dojo/webapp/handler/JsonHandler.java
+++ src/main/java/org/dojo/webapp/handler/JsonHandler.java
@@ -4,12 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
